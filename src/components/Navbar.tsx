3ebd8b99e35e4590f688eb6ef35529fc2aaa6f21
Bruce Jung <sunghunet@gmail.com>
2025-05-19 12:13:24 +0900

    chore: remove unused EditorJs import from Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { motion } from 'framer-motion';
 import { Rocket, Home, FileText, Mail, Shield } from 'lucide-react';
-import EditorJs from '@/components/EditorJs';
 
 const navItems = [
   { name: 'Home', href: '/', icon: Home },
